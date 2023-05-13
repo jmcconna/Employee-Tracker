44c70074fccbe84fb603efc389a79b2b4a138d46
James McConnaughey <jbmcconnaughey@gmail.com>
2023-05-13 15:02:36 -0400

    added SQL schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,26 @@
-DROP DATABASE IF EXISTS movie_db;
-CREATE movie_db;
+DROP DATABASE IF EXISTS company_db;
+CREATE company_db;
 
-USE movie_db;
+USE company_db;
 
-CREATE TABLE movies (
- id INT NOT NULL,
- movie_name VARCHAR(100) NOT NULL
+CREATE TABLE departments (
+ id: INT NOT NULL, 
+ departmentName: VARCHAR(30),
+PRIMARY KEY (id) 
 );
 
-CREATE TABLE reviews (
-    id INT NOT NULL,
-    movie_id INT NOT NULL,
-    review TEXT NOT NULL
+CREATE TABLE roles (
+    id: INT NOT NULL,
+    title: VARCHAR(30),
+    salary: DECIMAL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id: INT NOT NULL,
+    firstName: VARCHAR(30),
+    lastName: VARCHAR(30),
+    role_id: INT NOT NULL,
+    manager_id: INT,
+    PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,17 @@
-INSERT INTO movies (id, movie_name)
-VALUES (001, "Braveheart"),
-        (002, "Gladiator"),
-        (003, "Saving Private Ryan");
+INSERT INTO departments (id, departmentName)
+VALUES (001, "Operations"),
+        (002, "Engineering"),
+        (003, "Sales");
 
-INSERT INTO reviews (id, movie_id, review)
-VALUES (001, 001, "Too long"),
-        (002, 001, "Not enough action scenes"),
-        (003, 003, "Tom Hanks is an American treasure.");
+INSERT INTO roles (id, title, salary)
+VALUES (001, "CEO", 250000),
+        (002, "Account Executive", 90000),
+        (003, "Technician", 65000),
+        (004, "Project Manager", 120000),
+        (005, "Engineer", 85000);
+
+INSERT INTO employees (id, firstName, lastName, role_id, manager_id)
+VALUES (001, "Joe", "Smith", 001, NULL),
+        (002, "Bob", "Wig", 002, 001),
+        (003, "Sally", "Forth", 003, 001),
+        (004, "Candy", "Applebottom", 004, 001);
